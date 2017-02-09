9e4bb1fd69ed8bad7ca3ae8ab65956790a5b494b
JhonFRestrepo <jhonfrestrepo@gmail.com>
2017-02-08 22:14:11 -0500

    JFR: Adding the other menus
    Adding menus and scenes, along with some of the scripts.

---
+++ Assets/_FirstParty/Scripts/UI/UIInteractionsLoadMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIInteractionsLoadMenu : MonoBehaviour
+{
+  //===========================================================================
+  public void OnButtonLoadSLot1()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add load slot 1 function here");        
+  }
+
+  //===========================================================================
+  public void OnButtonDeleteSLot1()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add delete slot 1 function here");    
+  }
+
+  //===========================================================================
+  public void OnButtonLoadSLot2()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add load slot 2 function here");
+  }
+
+  //===========================================================================
+  public void OnButtonDeleteSLot2()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add delete slot 2 function here");
+  }
+
+  //===========================================================================
+  public void OnButtonLoadSLot3()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add load slot 3 function here");
+  }
+
+  //===========================================================================
+  public void OnButtonDeleteSLot3()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: Add delete slot 3 function here");
+  }
+
+  //===========================================================================
+  public void OnButtonPressedBack()
+  {
+    SceneManager.LoadScene("MainMenu");
+  }
+}
---
+++ Assets/_FirstParty/Scripts/UI/UIInteractionsMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIInteractionsMainMenu : MonoBehaviour
+{
+  //===========================================================================
+  public void OnButtonPressedEnterFloorPlan ()
+  {
+    //TODO: JFR: 
+    Debug.LogWarning("TODO: LOAD SCENE FLOOR PLAN HERE");
+  }
+
+  //===========================================================================
+  public void OnButtonPressedLoadSetUp ()
+  {
+    SceneManager.LoadScene("LoadMenu");
+  }
+
+  //===========================================================================
+  public void OnButtonPressedOptions()
+  {
+    SceneManager.LoadScene("OptionsMenu");
+  }
+}
