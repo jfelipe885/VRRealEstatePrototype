4e74b2873419e0bdfe2692fb8ef65572f9753a99
jfelipe885 <jfelipe885@gmail.com>
2017-05-16 21:40:36 -0500

    Vive and oculus integration

---
+++ Assets/_FirstParty/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject trackedObj;
+    // 1
+    private GameObject collidingObject;
+    // 2
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        Debug.Log("***trackedObj: " + trackedObj.isValid);
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<SteamVR_TrackedObject>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("***trackedObj: ");
+        // 1
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // 2
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<UnityEngine.Rigidbody>();
+    }
+
+    // 3
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<UnityEngine.FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<SteamVR_TrackedObject>())
+        {
+            // 2
+            GetComponent<UnityEngine.FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<UnityEngine.Rigidbody>());
+            // 3
+            objectInHand.GetComponent<UnityEngine.Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<UnityEngine.Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        // 4
+        objectInHand = null;
+    }
+}
+
---
+++ Assets/_FirstParty/Scripts/VRDeviceManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRDeviceManager : MonoBehaviour {
+    [SerializeField]
+    GameObject m_oculus_device = null;
+    [SerializeField]
+    GameObject m_vive_device = null;
+    [SerializeField]
+    GameObject m_gearvr_device = null;
+    [SerializeField]
+    GameObject m_other_device = null;
+    [SerializeField]
+    GameObject CameraRig = null;
+
+    public static String OCULUS_DEVICE = "Oculus Rift CV1";
+    public static String VIVE_DEVICE = "Vive MV";
+    public static String OTHER_DEVICE = "OpenVR";
+
+    // Use this for initialization
+    void Start () {
+
+        foreach (string s in VRSettings.supportedDevices) {
+            Debug.Log("Device: " + s);
+        }
+        Debug.Log("***Device: " + UnityEngine.VR.VRDevice.model);
+        if (String.Compare(UnityEngine.VR.VRDevice.model, OCULUS_DEVICE) == 0)
+        {
+            CameraRig = m_oculus_device;
+            Debug.Log("Oculus is Active: " + m_oculus_device.activeSelf);
+        }
+        else if (String.Compare(UnityEngine.VR.VRDevice.model, VIVE_DEVICE) == 0)
+        {
+            CameraRig = m_vive_device;
+            Debug.Log("Vive is Active: " + m_vive_device.activeSelf);
+        }
+        else if (String.Compare(UnityEngine.VR.VRDevice.model, OTHER_DEVICE) == 0)
+        {
+            CameraRig = m_other_device;
+            Debug.Log("Other VR is Active: " + m_other_device.activeSelf);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
